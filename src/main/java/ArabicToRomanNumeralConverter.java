8e5f1bfa7682b45285c458b2d88bde159dc1df87
Kyle Engan <Kyle Engan>
2017-02-15 19:18:12 -0500

    KE - Added some whitespace for readability

--- src/main/java/ArabicToRomanNumeralConverter.java
+++ src/main/java/ArabicToRomanNumeralConverter.java
@@ -14,10 +14,13 @@ public class ArabicToRomanNumeralConverter {
         try {
             int numberToConvert = Integer.parseInt(convertMe.replaceAll(",", ""));
             System.out.println(convertToRomanNumeral(numberToConvert));
+
         } catch (NumberFormatException nfe) {
             System.out.println("Error: '" + convertMe + "' is not an integer.  Please try again with a number between 1 and 3,999.");
+
         } catch (IllegalArgumentException iae) {
             System.out.println("Error: This tool can only convert numbers up to 3,999.  Please try again with a number between 1 and 3,999.");
+            
         }
     }
 
