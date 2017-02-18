de319fef5a433667c1b034b4ca799d968e0d2dbd
Kyle Engan <Kyle Engan>
2017-02-18 11:25:01 -0500

    KE - Test and code for converting IV

--- toArabic/src/main/java/RomanToArabicConverter.java
+++ toArabic/src/main/java/RomanToArabicConverter.java
@@ -3,11 +3,18 @@ public class RomanToArabicConverter {
 
     public int convert(String roman) {
         int returnVal = 0;
+
+        if (roman.contains("IV")) {
+            returnVal += 4;
+            roman = roman.replaceAll("IV", "");
+        }
+
         for (int c = 0; c < roman.length(); c++) {
             if (roman.charAt(c) == 'I') {
                 returnVal++;
             }
         }
+
         return returnVal;
     }
 
--- toArabic/src/test/java/RomanToArabicConverterTest.java
+++ toArabic/src/test/java/RomanToArabicConverterTest.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 import static org.junit.Assert.*;
 
@@ -18,5 +19,10 @@ public class RomanToArabicConverterTest {
         assertEquals(1, subject.convert("I"));
         assertEquals(2, subject.convert("II"));
         assertEquals(3, subject.convert("III"));
+    }
+
+    @Test
+    public void shouldConvertIVTo4() {
+        assertEquals(4, subject.convert("IV"));
     }
 }
