8e32def579ad8c10d135710cdfa23ec9bc0b86ab
Kyle Engan <Kyle Engan>
2017-02-19 14:24:41 -0500

    KE - Refactored Arabic to Roman test names for consistency

--- toRomanNumeral/src/test/java/ArabicToRomanNumeralConverterTest.java
+++ toRomanNumeral/src/test/java/ArabicToRomanNumeralConverterTest.java
@@ -26,7 +26,108 @@ public class ArabicToRomanNumeralConverterTest {
     }
 
     @Test
-    public void whenRunWithoutAnArgument_willOutputAnErrorToConsole() {
+    public void whenConvertingOnesDigit_givenANumberBetween0and9_shouldReturnCorrectRomanNumeral() {
+        assertEquals("", subject.convertArabicOnesDigitToRomanNumeral(0));
+        assertEquals("I", subject.convertArabicOnesDigitToRomanNumeral(1));
+        assertEquals("II", subject.convertArabicOnesDigitToRomanNumeral(2));
+        assertEquals("III", subject.convertArabicOnesDigitToRomanNumeral(3));
+        assertEquals("IV", subject.convertArabicOnesDigitToRomanNumeral(4));
+        assertEquals("V", subject.convertArabicOnesDigitToRomanNumeral(5));
+        assertEquals("VI", subject.convertArabicOnesDigitToRomanNumeral(6));
+        assertEquals("VII", subject.convertArabicOnesDigitToRomanNumeral(7));
+        assertEquals("VIII", subject.convertArabicOnesDigitToRomanNumeral(8));
+        assertEquals("IX", subject.convertArabicOnesDigitToRomanNumeral(9));
+    }
+
+    @Test
+    public void whenConvertingTensDigit_givenANumberBetween0and9_shouldReturnCorrectRomanNumeral() {
+        assertEquals("", subject.convertArabicTensDigitToRomanNumeral(0));
+        assertEquals("X", subject.convertArabicTensDigitToRomanNumeral(1));
+        assertEquals("XX", subject.convertArabicTensDigitToRomanNumeral(2));
+        assertEquals("XXX", subject.convertArabicTensDigitToRomanNumeral(3));
+        assertEquals("XL", subject.convertArabicTensDigitToRomanNumeral(4));
+        assertEquals("L", subject.convertArabicTensDigitToRomanNumeral(5));
+        assertEquals("LX", subject.convertArabicTensDigitToRomanNumeral(6));
+        assertEquals("LXX", subject.convertArabicTensDigitToRomanNumeral(7));
+        assertEquals("LXXX", subject.convertArabicTensDigitToRomanNumeral(8));
+        assertEquals("XC", subject.convertArabicTensDigitToRomanNumeral(9));
+    }
+
+    @Test
+    public void whenConvertingHundredsDigit_givenANumberBetween0and9_shouldReturnCorrectRomanNumeral() {
+        assertEquals("", subject.convertArabicHundredsDigitToRomanNumeral(0));
+        assertEquals("C", subject.convertArabicHundredsDigitToRomanNumeral(1));
+        assertEquals("CC", subject.convertArabicHundredsDigitToRomanNumeral(2));
+        assertEquals("CCC", subject.convertArabicHundredsDigitToRomanNumeral(3));
+        assertEquals("CD", subject.convertArabicHundredsDigitToRomanNumeral(4));
+        assertEquals("D", subject.convertArabicHundredsDigitToRomanNumeral(5));
+        assertEquals("DC", subject.convertArabicHundredsDigitToRomanNumeral(6));
+        assertEquals("DCC", subject.convertArabicHundredsDigitToRomanNumeral(7));
+        assertEquals("DCCC", subject.convertArabicHundredsDigitToRomanNumeral(8));
+        assertEquals("CM", subject.convertArabicHundredsDigitToRomanNumeral(9));
+    }
+
+    @Test
+    public void whenConvertingThousandsDigit_givenANumberBetween0and3_shouldReturnCorrectRomanNumeral() {
+        assertEquals("", subject.convertArabicThousandsDigitToRomanNumeral(0));
+        assertEquals("M", subject.convertArabicThousandsDigitToRomanNumeral(1));
+        assertEquals("MM", subject.convertArabicThousandsDigitToRomanNumeral(2));
+        assertEquals("MMM", subject.convertArabicThousandsDigitToRomanNumeral(3));
+    }
+
+    @Test
+    public void whenConvertingThousandsDigit_givenANumberrThan3_shouldThrowAnIllegalArgumentException() {
+        try {
+            subject.convertArabicThousandsDigitToRomanNumeral(4);
+            fail("Should have received an exception for a thousands digit > 3");
+        } catch (IllegalArgumentException iae) {
+            assertEquals("This tool can only convert numbers up to 3,999.  Please try again.", iae.getMessage());
+        } catch (Throwable t) {
+            fail("Unexpected throwable encountered: " + toString());
+        }
+    }
+
+    @Test
+    public void givenSelectedTwoDigitNumbers_shouldReturnCorrectValues() {
+        assertEquals("XI", subject.convertToRomanNumeral(11));
+        assertEquals("XXII", subject.convertToRomanNumeral(22));
+        assertEquals("XXXIII", subject.convertToRomanNumeral(33));
+        assertEquals("XLIV", subject.convertToRomanNumeral(44));
+        assertEquals("LV", subject.convertToRomanNumeral(55));
+        assertEquals("LXVI", subject.convertToRomanNumeral(66));
+        assertEquals("LXXVII", subject.convertToRomanNumeral(77));
+        assertEquals("LXXXVIII", subject.convertToRomanNumeral(88));
+        assertEquals("XCIX", subject.convertToRomanNumeral(99));
+    }
+
+    @Test
+    public void givenSelectedThreeDigitNumbers_shouldReturnCorrectValues() {
+        assertEquals("CXI", subject.convertToRomanNumeral(111));
+        assertEquals("CCXXII", subject.convertToRomanNumeral(222));
+        assertEquals("CCCXXXIII", subject.convertToRomanNumeral(333));
+        assertEquals("CDXLIV", subject.convertToRomanNumeral(444));
+        assertEquals("DLV", subject.convertToRomanNumeral(555));
+        assertEquals("DCLXVI", subject.convertToRomanNumeral(666));
+        assertEquals("DCCLXXVII", subject.convertToRomanNumeral(777));
+        assertEquals("DCCCLXXXVIII", subject.convertToRomanNumeral(888));
+        assertEquals("CMXCIX", subject.convertToRomanNumeral(999));
+    }
+
+    @Test
+    public void givenSelectedFourDigitNumbers_shouldReturnCorrectValues() {
+        assertEquals("MCXI", subject.convertToRomanNumeral(1111));
+        assertEquals("MMCCXXII", subject.convertToRomanNumeral(2222));
+        assertEquals("MMMCCCXXXIII", subject.convertToRomanNumeral(3333));
+        assertEquals("MMMCDXLIV", subject.convertToRomanNumeral(3444));
+        assertEquals("MMMDLV", subject.convertToRomanNumeral(3555));
+        assertEquals("MMMDCLXVI", subject.convertToRomanNumeral(3666));
+        assertEquals("MMMDCCLXXVII", subject.convertToRomanNumeral(3777));
+        assertEquals("MMMDCCCLXXXVIII", subject.convertToRomanNumeral(3888));
+        assertEquals("MMMCMXCIX", subject.convertToRomanNumeral(3999));
+    }
+
+    @Test
+    public void givenNoCommandLineArgument_willOutputAnErrorToConsole() {
         ArabicToRomanNumeralConverter.main(new String[0]);
 
         assertEquals("Error: No argument received.  Please run this utility again with a number between 1 and 3,999" + System.lineSeparator(),
@@ -34,7 +135,7 @@ public class ArabicToRomanNumeralConverterTest {
     }
 
     @Test
-    public void whenRunWithMoreThanOneArgument_willOutputAnErrorToConsole() {
+    public void givenMoreThanOneCommandLineArgument_willOutputAnErrorToConsole() {
         ArabicToRomanNumeralConverter.main(new String[2]);
 
         assertEquals("Error: Too many arguments received.  Please run this utility again with a number between 1 and 3,999" + System.lineSeparator(),
@@ -42,7 +143,7 @@ public class ArabicToRomanNumeralConverterTest {
     }
 
     @Test
-    public void whenRunWithANumberAsAString_willOutputResultToConsole() {
+    public void givenAStringContainingAnIntegerAsACommandLineArgument_willOutputResultToConsole() {
         String[] args = {"123"};
         ArabicToRomanNumeralConverter.main(args);
 
@@ -95,104 +196,4 @@ public class ArabicToRomanNumeralConverterTest {
         assertEquals("Error: This tool can only convert numbers up to 3,999.  Please try again with a number between 1 and 3,999." + System.lineSeparator(), outContent.toString());
     }
 
-    @Test
-    public void shouldReturnCorrectValueForOnesDigit() {
-        assertEquals("", subject.convertArabicOnesDigitToRomanNumeral(0));
-        assertEquals("I", subject.convertArabicOnesDigitToRomanNumeral(1));
-        assertEquals("II", subject.convertArabicOnesDigitToRomanNumeral(2));
-        assertEquals("III", subject.convertArabicOnesDigitToRomanNumeral(3));
-        assertEquals("IV", subject.convertArabicOnesDigitToRomanNumeral(4));
-        assertEquals("V", subject.convertArabicOnesDigitToRomanNumeral(5));
-        assertEquals("VI", subject.convertArabicOnesDigitToRomanNumeral(6));
-        assertEquals("VII", subject.convertArabicOnesDigitToRomanNumeral(7));
-        assertEquals("VIII", subject.convertArabicOnesDigitToRomanNumeral(8));
-        assertEquals("IX", subject.convertArabicOnesDigitToRomanNumeral(9));
-    }
-
-    @Test
-    public void shouldReturnCorrectValueForTensDigit() {
-        assertEquals("", subject.convertArabicTensDigitToRomanNumeral(0));
-        assertEquals("X", subject.convertArabicTensDigitToRomanNumeral(1));
-        assertEquals("XX", subject.convertArabicTensDigitToRomanNumeral(2));
-        assertEquals("XXX", subject.convertArabicTensDigitToRomanNumeral(3));
-        assertEquals("XL", subject.convertArabicTensDigitToRomanNumeral(4));
-        assertEquals("L", subject.convertArabicTensDigitToRomanNumeral(5));
-        assertEquals("LX", subject.convertArabicTensDigitToRomanNumeral(6));
-        assertEquals("LXX", subject.convertArabicTensDigitToRomanNumeral(7));
-        assertEquals("LXXX", subject.convertArabicTensDigitToRomanNumeral(8));
-        assertEquals("XC", subject.convertArabicTensDigitToRomanNumeral(9));
-    }
-
-    @Test
-    public void shouldReturnCorrectValuesForHundredsDigit() {
-        assertEquals("", subject.convertArabicHundredsDigitToRomanNumeral(0));
-        assertEquals("C", subject.convertArabicHundredsDigitToRomanNumeral(1));
-        assertEquals("CC", subject.convertArabicHundredsDigitToRomanNumeral(2));
-        assertEquals("CCC", subject.convertArabicHundredsDigitToRomanNumeral(3));
-        assertEquals("CD", subject.convertArabicHundredsDigitToRomanNumeral(4));
-        assertEquals("D", subject.convertArabicHundredsDigitToRomanNumeral(5));
-        assertEquals("DC", subject.convertArabicHundredsDigitToRomanNumeral(6));
-        assertEquals("DCC", subject.convertArabicHundredsDigitToRomanNumeral(7));
-        assertEquals("DCCC", subject.convertArabicHundredsDigitToRomanNumeral(8));
-        assertEquals("CM", subject.convertArabicHundredsDigitToRomanNumeral(9));
-    }
-
-    @Test
-    public void shouldReturnCorrectValuesForThousandsDigit() {
-        assertEquals("", subject.convertArabicThousandsDigitToRomanNumeral(0));
-        assertEquals("M", subject.convertArabicThousandsDigitToRomanNumeral(1));
-        assertEquals("MM", subject.convertArabicThousandsDigitToRomanNumeral(2));
-        assertEquals("MMM", subject.convertArabicThousandsDigitToRomanNumeral(3));
-    }
-
-    @Test
-    public void shouldThrowAnIllegalArgumentExceptionForAThousandsDigitGreaterThan3() {
-        try {
-            subject.convertArabicThousandsDigitToRomanNumeral(4);
-            fail("Should have received an exception for a thousands digit > 3");
-        } catch (IllegalArgumentException iae) {
-            assertEquals("This tool can only convert numbers up to 3,999.  Please try again.", iae.getMessage());
-        } catch (Throwable t) {
-            fail("Unexpected throwable encountered: " + toString());
-        }
-    }
-
-    @Test
-    public void shouldReturnCorrectValuesForSelectedTwoDigitNumbers() {
-        assertEquals("XI", subject.convertToRomanNumeral(11));
-        assertEquals("XXII", subject.convertToRomanNumeral(22));
-        assertEquals("XXXIII", subject.convertToRomanNumeral(33));
-        assertEquals("XLIV", subject.convertToRomanNumeral(44));
-        assertEquals("LV", subject.convertToRomanNumeral(55));
-        assertEquals("LXVI", subject.convertToRomanNumeral(66));
-        assertEquals("LXXVII", subject.convertToRomanNumeral(77));
-        assertEquals("LXXXVIII", subject.convertToRomanNumeral(88));
-        assertEquals("XCIX", subject.convertToRomanNumeral(99));
-    }
-
-    @Test
-    public void shouldReturnCorrectValuesForSelectedThreeDigitNumbers() {
-        assertEquals("CXI", subject.convertToRomanNumeral(111));
-        assertEquals("CCXXII", subject.convertToRomanNumeral(222));
-        assertEquals("CCCXXXIII", subject.convertToRomanNumeral(333));
-        assertEquals("CDXLIV", subject.convertToRomanNumeral(444));
-        assertEquals("DLV", subject.convertToRomanNumeral(555));
-        assertEquals("DCLXVI", subject.convertToRomanNumeral(666));
-        assertEquals("DCCLXXVII", subject.convertToRomanNumeral(777));
-        assertEquals("DCCCLXXXVIII", subject.convertToRomanNumeral(888));
-        assertEquals("CMXCIX", subject.convertToRomanNumeral(999));
-    }
-
-    @Test
-    public void shouldReturnCorrectValuesForSelectedFourDigitNumbers() {
-        assertEquals("MCXI", subject.convertToRomanNumeral(1111));
-        assertEquals("MMCCXXII", subject.convertToRomanNumeral(2222));
-        assertEquals("MMMCCCXXXIII", subject.convertToRomanNumeral(3333));
-        assertEquals("MMMCDXLIV", subject.convertToRomanNumeral(3444));
-        assertEquals("MMMDLV", subject.convertToRomanNumeral(3555));
-        assertEquals("MMMDCLXVI", subject.convertToRomanNumeral(3666));
-        assertEquals("MMMDCCLXXVII", subject.convertToRomanNumeral(3777));
-        assertEquals("MMMDCCCLXXXVIII", subject.convertToRomanNumeral(3888));
-        assertEquals("MMMCMXCIX", subject.convertToRomanNumeral(3999));
-    }
 }
